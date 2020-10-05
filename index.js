c4e57c149812c64a79753479e65b84349199faa0
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-10-04 18:38:59 -0700

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      for (let i = 0; i < newCollection.length; i++){
+        callback(newCollection[i])
+      }
 
+      return collection
     },
 
-    map: function() {
+    map: function(collection, callback) {
+      if (collection instanceof Array) {
+        for (let i = 0; i < collection.length; i++){
+          callback(collection[i])
+        }
+      } else {
+        for (let i = 0; i < collection.length; i++){
+          callback(collection[i], Object.values(collection[i]))
+        }
+      }
 
+      return collection
     },
 
     reduce: function() {
