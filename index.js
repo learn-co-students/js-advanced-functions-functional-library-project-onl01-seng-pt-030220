ffe66f976d3ee1b11c8489a9ebfa3c90ed9f3f2b
AHMED <eng.computer1@yahoo.com>
2021-01-07 10:03:29 -0600

    Done.

--- index.js
+++ index.js
@@ -1,202 +1,186 @@
 const fi = (function() {
   return {
     libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-
+      return "Start by reading the article!";
     },
-
+ 
     each: function(collection, callback) {
-            if (Array.isArray(collection))
-             {
-               for (let i=0;i<collection.length;i++)
-               {
-                  callback(collection[i],i,collection)
-                }
+    
+        if(Array.isArray(collection)){
+    
+            for(let i = 0 ; i < collection.length ; i++ ){
+                   callback(i,collection[i],collection)
+            }       
+          }
+          
+          else{
+             for(const el in collection){
+                  callback(collection[el],el,collection)
+              }    
 
+           
+          }
+          return collection;
+    },
+
+         
+        map: function(collection,callback) {
+        
+          
+            if(!Array.isArray(collection))
+            {    
+                collection = Object.values(collection)
+            }
+              let newArray=[]
+              for(let i= 0 ; i< collection.length;i++){
+                    newArray.push(callback(collection[i]))
+              }
+          
+          return newArray
+            
+        },
+
+        reduce: function(collection,callback,acc){
+          
+             if(! acc){
+               acc=collection[0]
+               collection=collection.slice(1)
              }
-             else
+             
+             for(let i= 0 ;i< collection.length;i++)
              {
-                for(const property in collection){
-                  callback(property,collection[property],collection)
+                acc=callback(acc,collection[i],collection)
+              }
+             
+             return acc   
+        },
+
+        find: function(collection,predicate){
+         let  newarr=[]
+
+            if( !Array.isArray(collection)){
+                
+                collection=Object.values(collection)
+            }
+        
+            for(let i = 0 ;i < collection.length ;i++){
+                if( predicate(collection[i])){
+                  return (collection[i])
                 }
+                
+            }   
+        } ,
+        filter: function(collection,predicate){
 
+              if(! Array.isArray(collection)){
+                collection=Object.values(collection)
+              }
+              let newArr=[]
+              for(let i=0 ;i< collection.length;i++){
+                      if(predicate(collection[i])){
+                        newArr.push(collection[i])
+                      }
+              }
+              return newArr
+
+        },
+
+        size: function(collection){
+          if(! Array.isArray(collection)){
+            collection=Object.values(collection)
+          }
+             return collection.length
+        },
+        first: function(collection,n){
+              
+            if(n){     
+                return collection.slice(0,n)
+             }else{
+               return collection[0]
+             }
+             
+        },
+        last:function(array,n){
+          if(n){
+             return array.slice(-n)
+          }else{
+              return array[array.length-1]
+          }
+
+        },
+        
+        compact: function(array){
+             let newArr=[]
+              for(let i= 0;i<array.length ;i++){
+                 if(array[i] == false  || array[i] == null || array[i] == undefined || array[i] == 0 || array[i] == "" ||  Number.isNaN( array[i])){
+                    array[i]
+                  }else{
+                      newArr.push( array[i])
+                  }
+
+              }
+              return newArr
+        },
+
+         sortBy: function(array,callback){
+                  let newarry=[]
+                  newarry=[...array]  
+                  return  newarry.sort((a,b) => callback(a)-callback(b))
+              
+         },
+         flatten: function(array,shallow){
+
+           if(shallow){
+             let arr=[]
+                  return  arr =[].concat(...array)
+              }else{
+                return  array.join().split(',').map(e=>parseInt(e))
+                //or using reduce and recursion
+                /* function deep(array) { return array.reduce((acc,element)=>
+                     Array.isArray(element)?  acc.concat(deep(element)):acc.concat(element)
+                  ,[])}
+                 return deep(array)
+                    } */
             }
 
-       return collection
-    },
+         },
+         uniq:   function(array, isSorted, callback){
+           let sorted=array.sort(function(a,b){return a-b})
+         if (isSorted){
+              let k=sorted.map(function(element,index,sorted){
+              return element === sorted[index+1] ? delete sorted[index]:element
+              }
+            ).filter(el => typeof(el) == "number")
+            return [... new Set(k)]
+          }else if (callback) {
+                 let newArray = array.map(el=> callback(el))
+                  return   [... new Set(newArray)]
+          }
+             return [... new Set(array)]
+         },
+         keys: function(object){
 
-    map: function(collection,callback) {
-        let newArray=[]
-          for(let property in collection){
-            if(Array.isArray(collection))
-            {
+             return Object.keys(object)
+         },
 
-                   newArray.push(callback(collection[property],property,collection))
+        values: function(object){
+
+             return Object.values(object)
+         },
+         functions: function(object) {
+            let array=[]
+
+            for(let i in object){
+
+                if (typeof(object[i]) === "function")
+                array.push(i)
             }
-            else
-            {
-                    newArray.push(callback(collection[property],property,collection))
-
-            }
-        }
-
-        return newArray
-    },
-
-  reduce: function(collection, callback, acc) {
-        if (acc)
-        {
-            return collection.reduce((acc,el) => {
-              return callback(acc,el,collection)
-            },acc)
-
-        }
-      else{
-
-            return  collection.reduce((acc,el) => {
-                return callback(acc,el,collection)
-            })
-
-      }
+          return   array.sort()
 
 
-  },
+        },
+  };
+})();
+ 
+fi.libraryMethod();
 
-  //fi.find
-
-  find: function(collection, predicate){
-
-     return collection.find(element =>   predicate(element))
-  },
-
-  filter: function(collection,predicate){
-
-    return collection.filter(element => predicate(element))
-  },
-
-  size: function(collection){
-   if(Array.isArray(collection)){
-     return   collection.length
-   }
-
-   else {
-     return  Object.keys(collection).length
-   }
-  },
-
-  first: function(collection,n){
-    if (n){
-      return collection.slice(0,n)
-    }else{
-
-      return parseInt(collection.slice(0,1))
-
-    }
-
-},
-
-
-
-last: function(collection,n){
-   if(n){
-     return collection.slice(-n,collection.length)
-   }else{
-
-       return parseInt(collection.slice(-1))
-   }
-
-
-},
-
-compact: function(collection){
- return collection.filter(function(e){ return (e !== false && e !== null
- && !Number.isNaN(e) && e !== "" && e !== 0 && e !== undefined)})
-},
-
-sortBy: function(array, callback){
-  let newArray=Array.from(array)
-  return newArray.sort(function(a,b){return callback(a)-callback(b)})
-},
-
-flatten: function(array, shallow=false){
-//array.flat() not working
-///one level flat
- let single =[].concat(...array)
-if (shallow == true){
-    return  single
-}
-else
-{
-
-function deep(array) { return array.reduce((accum,element)=>
- Array.isArray(element)?  accum.concat(deep(element)):accum.concat(element)
-,[])}
-
-return deep(array)
-}
-
-},
-
-uniq: function(array, isSorted=false, callback=false){
-
-  ///first way
-
-//  return [... new Set(array)]
-  //second way
-  //arr.filter((element,index)=> arr.indexOf(element) === index)
-   //third way
-  let sorted =array.sort(function(a,b){return a-b})//
-  if (isSorted){
-  let k=sorted.map(function(element,index,sorted){
-      return element === sorted[index+1] ? delete sorted[index]:element
-    }
-    ).filter(el => typeof(el) == "number")
-  return [... new Set(k)]
-}
-
-if(callback ){
-  //return [... new Set(array.filter(el => el%3).sort(function(a,b){return a-b}))]
-
-  let newArray = array.map(el=> callback(el))
-
-  return   [... new Set(newArray)]
-}
-//when applied %3
-return [... new Set(array)]
-
-},
-
-keys: function(object){
-
-  return Object.keys(object)
-},
-
-values: function(object){
-
-  return Object.values(object)
-},
-
-
-
-
-functions: function(object) {
-console.log(object)
-
-let array=[]
-
-for(let i in object){
-
- if (typeof(object[i]) === "function")
-    array.push(i)
-   }
-return   array.sort()
-
-
-},
-
-
-
-  }
-})()
-
-fi.libraryMethod()
